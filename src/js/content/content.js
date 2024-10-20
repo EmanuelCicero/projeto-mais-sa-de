bf55f6adbc958fd3595b7a272dedd1fc5f046f57
lucassec1 <lucasemmanuelcs@gmail.com>
2024-10-20 20:12:57 -0300

    feat: add content at the home

---
+++ src/js/content/content.js
@@ -0,0 +1,20 @@
+const Content = [
+  {
+    image: 'src/images/my-health/agendamentos.svg',
+    category: 'Meus Dados',
+    name: 'Agendamentos',
+    tag: 'Agendamentos',
+    info: 'Agende consultas e exames.',
+    url: 'https://meususdigital.saude.gov.br/consultas'
+  },
+  {
+    image: 'src/images/my-health/agendamentos.svg',
+    category: 'Meus Dados',
+    name: 'Agendamentos',
+    tag: 'Agendamentos',
+    info: 'Agende consultas e exames.',
+    url: 'https://meususdigital.saude.gov.br/consultas'
+  },
+];
+
+export default Content;
--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,8 @@ import AplicationService from "./aplications/aplicationService.js";
 import Aplications from "./aplications/aplication.js";
 import MyHealthService from "./my-health/myHealthService.js";
 import MyHealth from "./my-health/myHealth.js";
+import ContentService from "./content/contentService.js";
+import Content from "./content/content.js";
 
 const detailService = new DetailService('data_detail');
 for (const detail of Details) {
@@ -18,4 +20,9 @@ for (const aplication of Aplications) {
 const myHealthService = new MyHealthService('data_my-health');
 for (const myHealth of MyHealth) {
   myHealthService.renderMyHealth(myHealth);
+}
+
+const contentService = new ContentService('data_content');
+for (const content of Content) {
+  contentService.renderContent(content);
 }
